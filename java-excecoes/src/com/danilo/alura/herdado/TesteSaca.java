a6bd42dd184d3352525a9f5669de428c8fb0714c
Danilo Silva <dansilva41@hotmail.com>
2018-05-24 22:08:21 -0300

    Tratando excecao SaldoInsuficienteException

--- java-excecoes/src/com/danilo/alura/herdado/TesteSaca.java
+++ java-excecoes/src/com/danilo/alura/herdado/TesteSaca.java
@@ -1,13 +1,19 @@
 package com.danilo.alura.herdado;
 
+import com.danilo.alura.criacao.SaldoInsuficienteException;
+
 public class TesteSaca {
-	
+
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 
-        conta.deposita(200.0);
-        conta.saca(201.0);
-
-        System.out.println(conta.getSaldo());
+		conta.deposita(200.0);
+		try {
+			conta.saca(210.0);
+		} catch (SaldoInsuficienteException ex) {
+			System.out.println("Ex: " + ex.getMessage());
+		}
+		
+		System.out.println(conta.getSaldo());
 	}
 }
